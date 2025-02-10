d44799893a68e1a934d03be7f0236797558914cf
Chirag-2199 <chiragkumar2199@gmail.com>
2025-02-10 08:45:57 +0530

    updated dashboard to look cool

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// removed all rotues for clean up 
-// will add later accordingly and manageable content now things are gone out of hand for now will consider doing it first 
+// removed all rotues for clean up
+// will add later accordingly and manageable content now things are gone out of hand for now will consider doing it first
 
 
 // Protect dashboard route with authentication middleware
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
+
     })->name('dashboard');
 
     // Profile Routes
