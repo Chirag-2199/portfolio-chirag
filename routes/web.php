616208d6b56ea164d90278eafbcd4a2f6c1824ac
Chirag-2199 <chiragkumar2199@gmail.com>
2025-02-17 22:04:11 +0530

    solved error on prod

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
 Route::get('/about', function () {
